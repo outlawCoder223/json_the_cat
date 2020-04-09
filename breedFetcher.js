8e2332ec0d32f9958c6d04aa5500d6ecfb55f2ed
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 16:08:06 +0000

    Parses json from get request to log cat desription

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,16 @@
 const request = require('request');
+const breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-  console.log(typeof body);
-  console.log(body);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    console.log('Error :', error);
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("I couldn't find that breed!");
+  } else {
+    const description = data[0].description;
+    console.log(description);
+  }
+  
 });
